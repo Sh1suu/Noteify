edfd8d6a272f23ccd900c335664dd2be835ed998
LAB24_USER ON WS08 <L24X11W08@LabHCI.net>
2026-03-11 13:36:56 +0800

    Initial commit

--- app/src/main/java/edu/cit/gaviola/noteify/MainActivity.kt
+++ app/src/main/java/edu/cit/gaviola/noteify/MainActivity.kt
@@ -1,6 +1,9 @@
 package edu.cit.gaviola.noteify
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_login)
+
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+
+        btnSignIn.setOnClickListener {
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
